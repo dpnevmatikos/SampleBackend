73bbf9be62e9a7bb5d1f32263b6ac2c0b0d53dc3
dpnevmatikos <d.pnevmatikos@kaizengaming.com>
2022-10-04 14:50:04 +0300

    Add model and DbContext

---
+++ lib/SampleBackend.Data/SampleBackendDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SampleBackend.Model;
+
+namespace SampleBackend.Data
+{
+    public class SampleBackendDbContext : DbContext
+    {
+        public SampleBackendDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>()
+                .ToTable("Customer");
+        }
+    }
+}
---
+++ lib/SampleBackend.Model/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleBackend.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int? VatNumber { get; set; }
+        public DateTimeOffset RegistrationDate { get; set; }
+    }
+}
